4c934ab327601b07541c06eb2c4e0a0121094545
Chahal13 <59967600+Chahal13@users.noreply.github.com>
2020-02-19 18:21:03 -0500

    Update Practice1.java

--- src/practice1/Practice1.java
+++ src/practice1/Practice1.java
@@ -18,7 +18,7 @@ public class Practice1 {
         // TODO code application logic here
         
         
-        System.out.println("Hey, this is practice");
+        System.out.println("Hey  you , this is practice");
     }
     
 }
