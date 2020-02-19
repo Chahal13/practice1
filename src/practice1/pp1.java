ddb63fc96091c7e7d50ee614410d3eaeb271577a
Chahal13 <Chahal46sheridancollege.ca>
2020-02-19 17:27:57 -0500

    hi there

---
+++ src/practice1/pp1.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practice1;
+
+/**
+ *
+ * @author veerj
+ */
+public class pp1 {
+    int x;
+    int y;
+}
